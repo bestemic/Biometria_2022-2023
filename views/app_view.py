9659adb14e01ffd53acb3e4b407320569dffd8b8
bestemic <przemek.pawlik123@gmail.com>
2023-05-24 23:44:06 +0200

    Added all buttons in menu

--- views/app_view.py
+++ views/app_view.py
@@ -38,28 +38,42 @@ class AppView:
         start_label = Label(self.start_screen, text="Eigenfaces recognition app", font="Helvetica 50 bold", fg=LIGHT, bg=DARK)
         start_label.pack(pady=(120, 10))
 
-        button = Button(self.start_screen, text="Start", font=("Helvetica", 40), bg=DARK_BUTTON, fg=LIGHT,
-                        activebackground=DARK_BUTTON_FOCUS, activeforeground=LIGHT, bd=10, command=self.open_menu_screen)
+        button = Button(self.start_screen, text="Start", font=("Helvetica", 30), bg=DARK_BUTTON, fg=LIGHT,
+                        activebackground=DARK_BUTTON_FOCUS, activeforeground=LIGHT, bd=10, width=10, command=self.open_menu_screen)
         button.pack(pady=(150, 0))
 
     def open_menu_screen(self):
         self.menu_screen.tkraise()
 
     def init_menu_screen(self):
-        start_label = Label(self.menu_screen, text="Chooce option", font="Helvetica 50", fg=LIGHT, bg=DARK)
-        start_label.pack(pady=(70, 10))
+        menu_label = Label(self.menu_screen, text="Select option", font="Helvetica 45", fg=LIGHT, bg=DARK)
+        menu_label.pack(pady=(50, 75))        
 
-        # TODO name and function
-        login = Button(self.menu_screen, text="LOGIN", font=("Helvetica", 40), bg=DARK_BUTTON, fg=LIGHT,
-                        activebackground=DARK_BUTTON_FOCUS, activeforeground=LIGHT, bd=5, command=self.open_start_screen)
-        login.pack(pady=(100, 10))
+        button_container = Frame(self.menu_screen, bg=DARK)
+        button_container.pack()
 
-        # TODO name and function
-        register = Button(self.menu_screen, text="REGISTER", font=("Helvetica", 40), bg=DARK_BUTTON, fg=LIGHT,
-                        activebackground=DARK_BUTTON_FOCUS, activeforeground=LIGHT, bd=5, command=self.open_start_screen)
-        register.pack()
+        button_container_left = Frame(button_container, bg=DARK)
+        button_container_left.pack(side=LEFT, padx=(0, 20))
 
-        back = Button(self.menu_screen, text="Back", font=("Helvetica", 40), bg=DARK_BUTTON, fg=LIGHT,
-                        activebackground=DARK_BUTTON_FOCUS, activeforeground=LIGHT, bd=5, command=self.open_start_screen)
-        back.pack(pady=(10, 0))
+        button_container_right = Frame(button_container, bg=DARK)
+        button_container_right.pack(side=RIGHT, padx=(20, 0))
 
+        login_button1 = Button(button_container_left, text="Login by File", font=("Helvetica", 27), bg=DARK_BUTTON, fg=LIGHT,
+                           activebackground=DARK_BUTTON_FOCUS, activeforeground=LIGHT, bd=5, width=18, command=self.open_start_screen)
+        login_button1.pack(pady=20)
+
+        login_button2 = Button(button_container_right, text="Login by Camera", font=("Helvetica", 27), bg=DARK_BUTTON, fg=LIGHT,
+                            activebackground=DARK_BUTTON_FOCUS, activeforeground=LIGHT, bd=5, width=18, command=self.open_start_screen)
+        login_button2.pack(pady=20)
+
+        register_button1 = Button(button_container_left, text="Register by File", font=("Helvetica", 27), bg=DARK_BUTTON, fg=LIGHT,
+                                activebackground=DARK_BUTTON_FOCUS, activeforeground=LIGHT, bd=5, width=18, command=self.open_start_screen)
+        register_button1.pack(pady=20)
+
+        register_button2 = Button(button_container_right, text="Register by Camera", font=("Helvetica", 27), bg=DARK_BUTTON, fg=LIGHT,
+                                activebackground=DARK_BUTTON_FOCUS, activeforeground=LIGHT, bd=5, width=18, command=self.open_start_screen)
+        register_button2.pack(pady=20)
+
+        back_button = Button(self.menu_screen, text="Back", font=("Helvetica", 27), bg=DARK_BUTTON, fg=LIGHT,
+                         activebackground=DARK_BUTTON_FOCUS, activeforeground=LIGHT, bd=5, width=15, command=self.open_start_screen)
+        back_button.pack(pady=(50, 0))
