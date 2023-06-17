a97d7a62c3f98e24e4f0936e4b3003518a0045a6
bestemic <przemek.pawlik123@gmail.com>
2023-06-17 10:03:51 +0200

    Refactored code for image generation.

---
+++ logic/generator.py
@@ -0,0 +1,39 @@
+import os.path
+import shutil
+
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def generate():
+    if not os.path.exists('generated_faces'):
+        os.mkdir('generated_faces')
+
+    matplotlib.use('TkAgg')
+
+    data = np.load("olivetti_faces.npy")
+
+    j = 0
+    parent_dir = ""
+    for i in range(len(data)):
+        if j == 0:
+            parent_dir = os.path.join("generated_faces", 'person_' + (i // 10).__str__())
+            if os.path.exists(parent_dir):
+                shutil.rmtree(parent_dir)
+            os.mkdir(parent_dir)
+
+        plt.figure()
+        plt.axis('off')
+        plt.subplots_adjust(left=0, right=1, top=1, bottom=0)
+        plt.imshow(data[i], cmap='gray')
+        file_name = parent_dir + "/face_" + j.__str__() + ".png"
+        plt.savefig(file_name, bbox_inches='tight', pad_inches=0)
+        plt.close()
+
+        if j == 9:
+            j = 0
+        else:
+            j += 1
+
+    print("Images generated successfully")
--- main.py
+++ main.py
@@ -1,16 +1,13 @@
 import sys
 
-from helpers import Helper
+from logic.generator import generate
 from views.app_view import AppView
-
 
 if __name__ == '__main__':
     print("Starting Eigenfaces app")
-    helper = Helper()
 
-    if len(sys.argv) and (sys.argv[1] == "-generate"):
-        helper.generate()
+    if len(sys.argv) and (sys.argv[1] == "--generate"):
+        generate()
 
-    # view = AppView()
-
-    # view.display()
+    view = AppView()
+    view.display()
